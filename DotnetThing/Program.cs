6a28582b55d69d8f944452666446a3f4f85d739d
sdobbins90 <39285507+sdobbins90@users.noreply.github.com>
2022-05-10 15:32:59 -0400

    Initial commit

---
+++ DotnetThing/Program.cs
@@ -0,0 +1,19 @@
+﻿var fileName = "someText.txt";
+
+var current = 0;
+
+var path = Path.Join("data", fileName);
+
+if(File.Exists(path)) {
+    var content = File.ReadAllText(path);
+    current = int.Parse(content);
+}
+
+Console.WriteLine($"Current is {current}");
+
+while(true) {
+    Thread.Sleep(3000);
+    current = ++current;
+    File.WriteAllText(path, (current).ToString());
+    Console.WriteLine($"Wrote {current} at {DateTime.Now:t}");
+}
